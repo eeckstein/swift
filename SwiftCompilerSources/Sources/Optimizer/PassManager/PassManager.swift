b871e39d476d97d5a54b2fac7d6481a2a2d64b54
Erik Eckstein <eeckstein@apple.com>
2024-08-27 14:42:41 +0200

    - pass pipeline builder

---
+++ SwiftCompilerSources/Sources/Optimizer/PassManager/PassManager.swift
@@ -0,0 +1,86 @@
+//===--- PassManager.swift ------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import OptimizerBridging
+
+@resultBuilder
+struct FunctionPassPipelineBuilder {
+  typealias Component = [FunctionPass]
+  typealias Expression = FunctionPass
+
+  static func buildExpression(_ element: Expression) -> Component {
+    return [element]
+  }
+  static func buildExpression(_ element: Int) -> Component {
+    return []
+  }
+  static func buildOptional(_ component: Component?) -> Component {
+    guard let component = component else { return [] }
+    return component
+  }
+  static func buildEither(first component: Component) -> Component {
+    return component
+  }
+  static func buildEither(second component: Component) -> Component {
+    return component
+  }
+  static func buildArray(_ components: [Component]) -> Component {
+    return Array(components.joined())
+  }
+  static func buildBlock(_ components: Component...) -> Component {
+    return Array(components.joined())
+  }
+}
+
+@resultBuilder
+struct ModulePassPipelineBuilder {
+  typealias Component = [ModulePass]
+  typealias Expression = ModulePass
+
+  static func buildExpression(_ element: ModulePass) -> Component {
+    return [element]
+  }
+  static func buildExpression(_ element: [FunctionPass]) -> Component {
+    let pass = ModulePass(name: "function passes") {
+      runFunctionPasses(passes: element, $0)
+    }
+    return [pass]
+  }
+  static func buildOptional(_ component: Component?) -> Component {
+    guard let component = component else { return [] }
+    return component
+  }
+  static func buildEither(first component: Component) -> Component {
+    return component
+  }
+  static func buildEither(second component: Component) -> Component {
+    return component
+  }
+  static func buildArray(_ components: [Component]) -> Component {
+    return Array(components.joined())
+  }
+  static func buildBlock(_ components: Component...) -> Component {
+    return Array(components.joined())
+  }
+}
+
+func passPipeline(@FunctionPassPipelineBuilder _ passes: () -> [FunctionPass]) -> [FunctionPass] {
+  passes()
+}
+
+func passPipeline(@ModulePassPipelineBuilder _ passes: () -> [ModulePass]) -> [ModulePass] {
+  passes()
+}
+
+func runFunctionPasses(passes: [FunctionPass], _ context: ModulePassContext) {
+
+}
