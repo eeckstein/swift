cbba0f6852561ac3c6a965f01897d34e0f440881
Erik Eckstein <eeckstein@apple.com>
2025-05-09 14:04:43 +0200

    Swift AST/SIL: add Type APIs for `Builtin.FixedArray`
    * `Type.isBuiltinFixedArray`
    * `Type.builtinFixedArrayElementType`

--- SwiftCompilerSources/Sources/SIL/Type.swift
+++ SwiftCompilerSources/Sources/SIL/Type.swift
@@ -82,6 +82,10 @@ public struct Type : TypeProperties, CustomStringConvertible, NoReflectionChildr
 
   public var builtinVectorElementType: Type { canonicalType.builtinVectorElementType.silType! }
 
+  public func builtinFixedArrayElementType(in function: Function) -> Type {
+    canonicalType.builtinFixedArrayElementType.loweredType(in: function)
+  }
+
   public var superClassType: Type? { canonicalType.superClassType?.silType }
 
   public func isExactSuperclass(of type: Type) -> Bool {
