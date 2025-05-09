5c69abd61751c7b69b02c51c749f873b3082c558
Erik Eckstein <eeckstein@apple.com>
2025-05-09 14:19:56 +0200

    SIL: add the `vector_base_addr` instruction
    It derives the address of the first element of a vector, i.e. a `Builtin.FixedArray`, from the address of the vector itself.
    Addresses of other vector elements can then be derived with `index_addr`.

--- include/swift/SIL/SILBuilder.h
+++ include/swift/SIL/SILBuilder.h
@@ -1923,6 +1923,15 @@ public:
     return createStructElementAddr(Loc, Operand, Field, ResultTy);
   }
 
+  VectorBaseAddrInst *
+  createVectorBaseAddr(SILLocation loc, SILValue vector) {
+    auto arrayTy = vector->getType().getAs<BuiltinFixedArrayType>();
+    ASSERT(arrayTy && "operand of vector_extract must be a builtin array type");
+    auto elemtTy = getFunction().getLoweredType(arrayTy->getElementType());
+    return insert(new (getModule()) VectorBaseAddrInst(
+        getSILDebugLocation(loc), vector, elemtTy.getAddressType()));
+  }
+
   RefElementAddrInst *createRefElementAddr(SILLocation Loc, SILValue Operand,
                                            VarDecl *Field, SILType ResultTy,
                                            bool IsImmutable = false) {
