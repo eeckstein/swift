59841e3d299df76e28c387e953b54ac3871413a5
Erik Eckstein <eeckstein@apple.com>
2026-05-20 15:43:39 +0200

    - setValueHint -> rle

--- include/swift/SIL/SILBridgingImpl.h
+++ include/swift/SIL/SILBridgingImpl.h
@@ -1379,7 +1379,7 @@ SwiftInt BridgedInstruction::LoadInst_getLoadOwnership() const {
   return (SwiftInt)getAs<swift::LoadInst>()->getOwnershipQualifier();
 }
 
-bool BridgedInstruction::LoadBorrow_setValueHint(BridgedValue v) const {
+void BridgedInstruction::LoadBorrow_setValueHint(BridgedValue v) const {
   getAs<swift::LoadBorrowInst>()->setValueHint(v.getSILValue());
 }
 
